c684ac5b397ad0e5e8d8af898acaacd52ccc4662
jmayorga <jmayorgadev@gmail.com>
2017-11-24 11:17:07 -0500

    from section 77 to 92

---
+++ src/App.js
@@ -0,0 +1,26 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import { Text, View } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import reducers from './reducers';
+import { Header } from './components/common';
+import LibraryList from './components/LibraryList';
+
+const App = () =>{
+    return (
+      <Provider store={createStore(reducers)}>
+        <View style={{flex:1}}>
+          <Header headerText="Tech Stack" />
+          <LibraryList />
+        </View>
+      </Provider>
+    );
+  }
+
+export default App;
