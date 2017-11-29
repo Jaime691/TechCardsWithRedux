7583d2d5ddf15370b3de4100ae4c3354a64cfe2d
jmayorga <jmayorgadev@gmail.com>
2017-11-29 13:08:23 -0500

    final commit

--- src/App.js
+++ src/App.js
@@ -4,23 +4,23 @@
  * @flow
  */
 
-import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import React from 'react';
+import { View } from 'react-native';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
 import reducers from './reducers';
 import { Header } from './components/common';
 import LibraryList from './components/LibraryList';
 
-const App = () =>{
+const App = () => {
     return (
       <Provider store={createStore(reducers)}>
-        <View style={{flex:1}}>
+        <View style={{ flex: 1 }}>
           <Header headerText="Tech Stack" />
           <LibraryList />
         </View>
       </Provider>
     );
-  }
+  };
 
 export default App;
--- src/components/LibraryList.js
+++ src/components/LibraryList.js
@@ -26,8 +26,7 @@ class LibraryList extends Component {
     }
 }
 
-const mapStateToProps = state => {
-    return { libraries: state.libraries };
-};
+const mapStateToProps = state => ({ libraries: state.libraries });
+
 
 export default connect(mapStateToProps)(LibraryList);
